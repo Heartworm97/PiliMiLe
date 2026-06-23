5797cc14591c2011209acb4d863e8d4456627d44
月光下的黑驴子 <macbookpro@macBookdeMacBook-Pro.local>
2026-06-23 23:02:28 +0800

    chore: lib/pages/douban_video/detail/widgets/source_selector.dart

--- lib/pages/douban_video/detail/widgets/source_selector.dart
+++ lib/pages/douban_video/detail/widgets/source_selector.dart
@@ -17,7 +17,7 @@ class SourceSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final available = sources.where((s) => s.decodeStatus == '1').length;
+    final available = sources.where((s) => s.decodeStatus != '2').length;
     final total = sources.length;
 
     return Padding(
@@ -45,7 +45,7 @@ class SourceSelector extends StatelessWidget {
               itemBuilder: (context, index) {
                 final src = sources[index];
                 final isSelected = index == selectedIndex;
-                final isAvailable = src.decodeStatus == '1';
+                final isAvailable = src.decodeStatus != '2';
 
                 return ChoiceChip(
                   label: Text(
