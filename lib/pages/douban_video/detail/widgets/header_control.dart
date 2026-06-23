f5a71edd376d11100f144546933a97056986f192
月光下的黑驴子 <macbookpro@macBookdeMacBook-Pro.local>
2026-06-23 19:21:28 +0800

    chore: lib/pages/douban_video/detail/widgets/header_control.dart

--- lib/pages/douban_video/detail/widgets/header_control.dart
+++ lib/pages/douban_video/detail/widgets/header_control.dart
@@ -19,20 +19,21 @@ class DoubanVideoHeaderControl extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       child: Row(
         children: [
-          Obx(() {
-            final isFullScreen = plPlayerController.isFullScreen.value;
-            return _buildBtn(
-              tooltip: isFullScreen ? '退出全屏' : '返回',
-              icon: Icon(
-                isFullScreen ? Icons.fullscreen_exit : FontAwesomeIcons.arrowLeft,
-                size: isFullScreen ? 20 : 15,
-                color: Colors.white,
-              ),
-              onPressed: isFullScreen
-                  ? () => plPlayerController.triggerFullScreen(status: false)
-                  : Get.back,
-            );
-          }),
+          _buildBtn(
+            tooltip: '返回',
+            icon: const Icon(
+              FontAwesomeIcons.arrowLeft,
+              size: 15,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              if (plPlayerController.isFullScreen.value) {
+                plPlayerController.triggerFullScreen(status: false);
+              } else {
+                Get.back();
+              }
+            },
+          ),
           Obx(() {
             final isPortrait =
                 MediaQuery.of(context).orientation == Orientation.portrait;
