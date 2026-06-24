a5519d54b1d2e659625806c4f06a0fb070410629
月光下的黑驴子 <macbookpro@macBookdeMacBook-Pro.local>
2026-06-25 01:53:18 +0800

    chore: lib/pages/douban_video/detail/widgets/episode_selector.dart

--- lib/pages/douban_video/detail/widgets/episode_selector.dart
+++ lib/pages/douban_video/detail/widgets/episode_selector.dart
@@ -317,7 +317,7 @@ class _DoubanEpisodePanelState extends State<_DoubanEpisodePanel>
                   tooltip: '正序',
                   icon: Icon(
                     Icons.vertical_align_top,
-                    size: 24,
+                    size: 20,
                     color: _isReversed ? null : theme.colorScheme.primary,
                   ),
                   onPressed: () => _setOrder(false),
@@ -327,7 +327,7 @@ class _DoubanEpisodePanelState extends State<_DoubanEpisodePanel>
                   tooltip: '倒序',
                   icon: Icon(
                     Icons.vertical_align_bottom,
-                    size: 24,
+                    size: 20,
                     color: _isReversed ? theme.colorScheme.primary : null,
                   ),
                   onPressed: () => _setOrder(true),
