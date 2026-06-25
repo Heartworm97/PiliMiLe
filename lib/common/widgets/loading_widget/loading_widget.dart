8134c238b621e98a61d1271941e0d8cc586340c3
月光下的黑驴子 <macbookpro@macBookdeMacBook-Pro.local>
2026-06-25 12:05:59 +0800

    chore: lib/common/widgets/loading_widget/loading_widget.dart

--- lib/common/widgets/loading_widget/loading_widget.dart
+++ lib/common/widgets/loading_widget/loading_widget.dart
@@ -1,8 +1,15 @@
 import 'package:PiliMiLe/common/widgets/loading_widget/http_error.dart';
 import 'package:PiliMiLe/common/widgets/loading_widget/m3e_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:material_new_shapes/material_new_shapes.dart';
 
-const Widget m3eLoading = Center(child: M3ELoadingIndicator());
+final Widget m3eLoading = Center(
+  child: M3ELoadingIndicator(
+    morphs: [
+      Morph(MaterialShapes.softBurst, MaterialShapes.softBurst),
+    ],
+  ),
+);
 
 const Widget linearLoading = SliverToBoxAdapter(
   child: LinearProgressIndicator(),
